8164602edd51785a55ed5c187d4e9210528019c8
ether <etherzhhb@gmail.com>
2012-12-02 09:35:50 +0800

    Minor change on delay distribution parsing script.

--- util/parse-delay-distribution.py
+++ util/parse-delay-distribution.py
@@ -3,7 +3,7 @@ import sys
 import json
 
 #period = 10
-resolution = 50
+resolution = 19
 
 max_distribution = {}
 mim_distribution = {}
@@ -22,26 +22,27 @@ for line in sys.stdin:
   #{u'SRC': u'0xad2f41c', u'DST': u'0xb68373c', u'LSB': 0.082799999999999999, u'MSB': 0.082799999999999999, u'MAX': 0.082799999999999999}
   chainDelay = json.loads(line[len("DELAY-ESTIMATION-JSON:"):])
 
-
   # Get the slowest bit delay.
   total_max_delay = total_max_delay + chainDelay["MAX"];
 
-  range_lb = int(chainDelay["MAX"] / resolution);
+  max_delay = int(chainDelay["MAX"])
+  range_lb = max_delay / resolution;
   max_distribution[range_lb] =  max_distribution.get(range_lb, 0) + 1
   maxlb = max(range_lb, maxlb)
 
   # Get the fastest bit delay.
-  range_lb = int(min(chainDelay["MSB"], chainDelay["LSB"]) / resolution);
+  min_delay = int(min(chainDelay["MSB"], chainDelay["LSB"]))
+  range_lb = min_delay / resolution;
   mim_distribution[range_lb] =  mim_distribution.get(range_lb, 0) + 1
   maxlb = max(range_lb, maxlb)
 
-  print "Built %d entires" % maxlb
+  print "Built %d entires: max %d min %d" % (maxlb, max_delay, min_delay)
   num_paths = num_paths + 1
 #print distribution
 
 print "Built %d entires" % maxlb
 
-for lb in range(0, maxlb):
+for lb in range(0, maxlb + 1):
   max_count = max_distribution.get(lb, 0)
   min_count = mim_distribution.get(lb, 0)
   print "[%dll,%dll)\t%d\t%f\t%d\t%f" % (lb * resolution, (lb + 1) *  resolution, max_count, float(max_count)/float(num_paths), min_count, float(min_count)/float(num_paths))
