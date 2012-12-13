61de38248b38f5d1d4c2d197a431f2b42a4e6b33
Swathi <swathi@swathi-desktop.(none)>
2012-12-13 10:21:04 +0800

    Reporting paths - HB added.

--- testsuite/extract_timing.tcl
+++ testsuite/extract_timing.tcl
@@ -1,6 +1,9 @@
 load_package report
 load_report
 
+#Output the false path.
+report_timing -from_clock { clk } -to_clock { clk } -setup -npaths 1 -detail full_path -stdout
+
 qsta_utility::generate_all_summary_tables
 report_clock_fmax_summary -panel_name "Fmax Summary"
 
