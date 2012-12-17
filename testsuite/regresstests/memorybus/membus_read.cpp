7464eb592cc0951e2d245dd82b1155b762370337
ether <etherzhhb@gmail.com>
2012-12-17 13:32:02 +0800

    Fix the expected result of memory read.

--- testsuite/regresstests/memorybus/membus_read.cpp
+++ testsuite/regresstests/memorybus/membus_read.cpp
@@ -4,17 +4,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int membus_read(int *addr) __attribute__ ((noinline));
+int membus_read(void *addr) __attribute__ ((noinline));
 
-int membus_read(int *addr) {
-  return *addr;
+int membus_read(void *addr) {
+  return *(int *)addr;
 }
 
 #ifdef __cplusplus
 }
 #endif
 
-#define exp_res 30836
 int main()
 {
   static int array0[NUM] = {61251, 39938, 557, 44087, 47457, 34892, 10951, 27320, 52441, 19223, 37907, 17733, 7955, 63768, 28004, 49123, 8207, 12611, 65416, 5149, 57091, 60947, 38247, 547, 49697, 41377, 18871, 18062, 28699, 44195, 62595, 22690, 9267, 127, 35116, 22432, 22036, 49858, 38900, 17577, 23766, 17334, 36673, 63610, 26343, 63308, 43736, 31387, 38191, 22004, 26629, 15195, 39604, 13415, 41537, 11555, 9773, 22456, 33278, 23191, 41678, 27438, 6844, 36947};
@@ -30,6 +29,6 @@ int main()
     result += membus_read(array0 + i);
   }
 
-//  printf("%ld\n", result);
-  return result != 30836;
+  //printf("%ld\n", result);
+  return result != 23539;
 }
