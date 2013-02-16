976ae0abdfbb82510e295f07602ebdc294c68e2b
ether <etherzhhb@gmail.com>
2013-02-17 01:12:25 +0800

    Add the new file for the LowerIntrinsic pass.

---
+++ lib/IROpts/LowerIntrinsic.cpp
@@ -0,0 +1,13 @@
+//===---- LowerIntrinsic.cpp - Lower the Intrinsics Functions ---*- C++ -*-===//
+//
+//                      The Shang HLS frameowrk                               //
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the LowerIntrinsic pass.
+//
+//===----------------------------------------------------------------------===//
+#include "shang/Passes.h"
