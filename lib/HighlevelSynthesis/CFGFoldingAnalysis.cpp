a73ff03436c774dae91772ec95d3fccb4187e67f
ether <etherzhhb@gmail.com>
2013-03-01 14:11:32 +0800

    Add the source file for  CFGFoldingAnalysis.

---
+++ lib/HighlevelSynthesis/CFGFoldingAnalysis.cpp
@@ -0,0 +1,13 @@
+//===- CFGFoldingAnalysis - Predict the Impact of CFGFolding ------ C++ ---===//
+//
+//                      The Shang HLS frameowrk                               //
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implement the CFGFlodingAnalysis, which try to predict the impcat
+// of CFG folding, e.g. generate big MUXs for the operation that get folded.
+//
+//===----------------------------------------------------------------------===//
