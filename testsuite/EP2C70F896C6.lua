d73a89572508f15720709aa54c2cff3b7f6b592a
ether <etherzhhb@gmail.com>
2013-03-06 20:56:12 +0800

    Fix the cyclone II constraints.

--- testsuite/EP2C70F896C6.lua
+++ testsuite/EP2C70F896C6.lua
@@ -3,12 +3,12 @@ FUs.LUTDelay = 0.4 / PERIOD
 FUs.BRam.Latency = 1.0 / PERIOD -- Block RAM
 
 -- Latency table for EP2C35F672C6
-FUs.AddSub   .Latencies = { 2.113 / PERIOD, 2.700 / PERIOD, 4.052 / PERIOD, 6.608 / PERIOD }
-FUs.Shift    .Latencies = { 3.073 / PERIOD, 3.311 / PERIOD, 4.792 / PERIOD, 5.829 / PERIOD }
-FUs.Mult     .Latencies = { 2.181 / PERIOD, 2.504 / PERIOD, 6.503 / PERIOD, 9.221 / PERIOD }
-FUs.ICmp     .Latencies = { 1.909 / PERIOD, 2.752 / PERIOD, 4.669 / PERIOD, 7.342 / PERIOD }
-FUs.Sel      .Latencies = { 0.835 / PERIOD, 1.026 / PERIOD, 1.209 / PERIOD, 2.769 / PERIOD }
-FUs.Reduction.Latencies = { 1.587 / PERIOD, 1.749 / PERIOD, 2.318 / PERIOD, 2.655 / PERIOD }
+FUs.Shift.Latencies = { 0.912 / PERIOD, 2.922 / PERIOD, 3.612 / PERIOD, 4.623 / PERIOD, 5.537 / PERIOD } --Shift
+FUs.AddSub.Latencies = { 1.066 / PERIOD, 2.139 / PERIOD, 2.835 / PERIOD, 4.138 / PERIOD, 6.753 / PERIOD } --Add
+FUs.Mult.Latencies = { 0.908 / PERIOD, 2.181 / PERIOD, 2.504 / PERIOD, 6.580 / PERIOD, 9.377 / PERIOD } --Mul
+FUs.Sel.Latencies = { 1.053 / PERIOD, 1.193 / PERIOD, 1.233 / PERIOD, 1.500 / PERIOD, 2.758 / PERIOD } --Sel
+FUs.Reduction.Latencies = { 0.785 / PERIOD, 1.597 / PERIOD, 1.744 / PERIOD, 2.172 / PERIOD, 2.733 / PERIOD } --Red
+FUs.ICmp.Latencies = { 0.912 / PERIOD, 1.906 / PERIOD, 2.612 / PERIOD, 4.050 / PERIOD, 6.660 / PERIOD } --Cmp
 FUs.Mux.Latencies = { 1.296 / PERIOD,
                       1.353 / PERIOD,
                       1.943 / PERIOD,
