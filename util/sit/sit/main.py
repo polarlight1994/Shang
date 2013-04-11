9d36e939fe6da53b948f9d686b08340b28da8d5b
ether <etherzhhb@gmail.com>
2013-04-11 17:27:09 +0800

    Identify the basedir for the tests.

--- util/sit/sit/main.py
+++ util/sit/sit/main.py
@@ -12,16 +12,23 @@ def ParseOptions() :
   parser = argparse.ArgumentParser(description='The Shang Integrated Tester')
   parser.add_argument("-m", "--mode", type=str, choices=["trivial"], help="the mode of sit", required=True)
   parser.add_argument("--tests", type=str, help="tests to run", required=True)
+  #parser.add_argument("--configs", type=str, help="config files", required=True)
 
   return parser.parse_args()
 
- def main(builtinParameters = {}):
+def loadConfig(configs, dst_dir) :
+  print "Load config"
+
+def main(builtinParameters = {}):
   args = ParseOptions()
 
   print "Starting the Shang Integrated Tester in", args.mode, "mode..."
 
-  for test in args.tests.split() :
-    print "Running", args.mode, "test on", test
+  for test_path in args.tests.split() :
+    basedir = os.path.dirname(test_path)
+    test_file = os.path.basename(test_path)
+    print "Running", args.mode, "test in", basedir
+    #Generate the synthesis configuration
 
 if __name__=='__main__':
     main()
