1c673e8d750e3f1bd52a864ded46609b44e2cbb1
ether <etherzhhb@gmail.com>
2013-04-11 14:04:23 +0800

    Add the Shang Integrated Tester.

---
+++ util/sit/sit/__init__.py
@@ -0,0 +1,10 @@
+"""Shang Integrated Tester"""
+
+from main import main
+
+__author__ = 'Hongbin Zheng'
+__email__ = 'etherzhhb@gmail.com'
+__versioninfo__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __versioninfo__)) + 'dev'
+
+__all__ = []
---
+++ util/sit/sit/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+sit - Shang Integrated Tester.
+
+"""
+
+import math, os, platform, random, re, sys, time, threading, traceback
+
+def main(builtinParameters = {}):
+  print "Hello from Shang Integrated Tester"
+
+if __name__=='__main__':
+    main()
