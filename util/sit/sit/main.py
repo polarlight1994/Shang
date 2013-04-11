19210f27438f53c402f36e2badc5951f629b01e4
ether <etherzhhb@gmail.com>
2013-04-11 17:05:31 +0800

    Add argument parser to SIT.

--- util/sit/sit/main.py
+++ util/sit/sit/main.py
@@ -7,8 +7,21 @@ sit - Shang Integrated Tester.
 
 import math, os, platform, random, re, sys, time, threading, traceback
 
-def main(builtinParameters = {}):
-  print "Hello from Shang Integrated Tester"
+def ParseOptions() :
+  import argparse
+  parser = argparse.ArgumentParser(description='The Shang Integrated Tester')
+  parser.add_argument("-m", "--mode", type=str, choices=["trivial"], help="the mode of sit", required=True)
+  parser.add_argument("--tests", type=str, help="tests to run", required=True)
+
+  return parser.parse_args()
+
+ def main(builtinParameters = {}):
+  args = ParseOptions()
+
+  print "Starting the Shang Integrated Tester in", args.mode, "mode..."
+
+  for test in args.tests.split() :
+    print "Running", args.mode, "test on", test
 
 if __name__=='__main__':
     main()
