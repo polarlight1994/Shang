0a1dd1f660f73d09974b70933c9824dc000c596b
ether <etherzhhb@gmail.com>
2013-04-12 12:00:39 +0800

    Add code to start the gridengine.

--- util/sit/sit/main.py
+++ util/sit/sit/main.py
@@ -6,6 +6,7 @@ sit - Shang Integrated Tester.
 """
 
 import math, os, platform, random, re, sys, time, threading, traceback
+import drmaa
 
 def ParseOptions() :
   import argparse
@@ -14,30 +15,52 @@ def ParseOptions() :
   parser.add_argument("--tests", type=str, help="tests to run", required=True)
   parser.add_argument("--tests_base", type=str, help="base dir of the test suit (to locate the config templates)", required=True)
   parser.add_argument("--ptr_size", type=int, help="pointer size in bits", required=True)
-  #parser.add_argument("--configs", type=str, help="config files", required=True)
+  parser.add_argument("--shang", type=str, help="path to shang executable", required=True)
 
   return parser.parse_args()
 
-def loadConfig(config_dir, dst_dir, test_config) :
-  from jinja2 import Environment, FileSystemLoader
-
-  env = Environment(loader=FileSystemLoader(config_dir))
-
-  env.filters['joinpath'] = lambda list: os.path.join(*list)
-
-  template= env.get_template('test_config.lua.in')
+def generateHLSConfig(test_name, dst_dir_base, test_config, template_env) :
+  # Create the local folder for the current test.
+  from datetime import datetime
+  dst_dir = os.path.join(dst_dir_base, test_name, datetime.now().strftime("%Y%m%d-%H%M%S-%f"))
+  os.makedirs(dst_dir)
 
   local_config = test_config.copy()
   local_config['test_binary_root'] = dst_dir
   #TODO: Scan the fmax.
   local_config['fmax'] = 100.0
 
-  print template.render(local_config)
+  synthesis_config = os.path.join(dst_dir, 'test_config.lua')
+  template_env.get_template('test_config.lua.in').stream(local_config).dump(synthesis_config)
+  yield synthesis_config
+
+def runHLS(session, shang, hls_config) :
+  # Create the HLS job.
+  jt = session.createJobTemplate()
+  jt.remoteCommand = shang
+  jt.args = [hls_config]
+  jt.joinFiles=True
+
+  jobid = session.runJob(jt)
+  session.deleteJobTemplate(jt)
+
+  return jobid
 
 def main(builtinParameters = {}):
   args = ParseOptions()
 
   print "Starting the Shang Integrated Tester in", args.mode, "mode..."
+
+  # Get the synthesis configuration templates.
+  from jinja2 import Environment, FileSystemLoader
+
+  env = Environment(loader=FileSystemLoader(args.tests_base))
+  env.filters['joinpath'] = lambda list: os.path.join(*list)
+
+  # Initialize the gridengine
+  s = drmaa.Session()
+  s.initialize()
+  hls_jobs = []
 
   for test_path in args.tests.split() :
     basedir = os.path.dirname(test_path)
@@ -51,8 +74,19 @@ def main(builtinParameters = {}):
                     "config_dir" : args.tests_base,
                     "ptr_size" : args.ptr_size}
 
-    #Generate the synthesis configuration
-    loadConfig(args.tests_base, basedir, test_config)
+    #Generate the synthesis configuration and run the test
+    for hls_config in generateHLSConfig(test_name, basedir, test_config, env) :
+      hls_jobs.append(runHLS(s, args.shang, hls_config))
+
+  # Wait untill all HLS jobs finish
+  s.synchronize(hls_jobs, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+  for curjob in hls_jobs:
+    print 'Collecting job ' + curjob
+    retval = s.wait(curjob, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+    print 'Job: ' + str(retval.jobId) + ' finished with status ' + str(retval.hasExited)
+
+  # Finialize the gridengine
+  s.exit()
 
 if __name__=='__main__':
     main()
