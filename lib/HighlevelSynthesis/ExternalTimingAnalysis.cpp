7a27fa0d007cf85727e4c21d3d76ba55998e5f3c
ether <etherzhhb@gmail.com>
2013-05-09 14:14:33 +0800

    ExternalTimingAnalysis: Add code to enumerate the possible paths in the combiantional cone.

--- lib/HighlevelSynthesis/ExternalTimingAnalysis.cpp
+++ lib/HighlevelSynthesis/ExternalTimingAnalysis.cpp
@@ -22,6 +22,7 @@
 
 #include "shang/Passes.h"
 #include "shang/VASTModule.h"
+#include "shang/VASTSubModules.h"
 
 #include "llvm/ADT/SetOperations.h"
 #include "llvm/Support/PathV1.h"
@@ -78,16 +79,10 @@ struct ExternalTimingAnalysis : public VASTModulePass {
   void writeProjectScript(raw_ostream &O, const sys::Path &NetlistPath,
                           const sys::Path &ExtractScript) const;
 
-  void extractTimingForPair(raw_ostream &O, const VASTSeqValue *Dst,
-                            const VASTValue *Thu,
-                            const VASTSeqValue *Src) const;
-
-  void extractTimingToDst(raw_ostream &O, const VASTSeqValue *Dst,
-                          const VASTValue *Thu, const SrcDelayInfo &Paths) const;
-
   // Write the script to extract the timing analysis results from quartus.
   void writeTimingExtractionScript(raw_ostream &O,
                                    const sys::Path &ResultPath) const;
+
   // Read the JSON file written by the timing extraction script.
   bool readTimingAnalysisResult(const sys::Path &ResultPath);
   bool readPathDelay(yaml::MappingNode *N);
@@ -102,7 +97,6 @@ struct ExternalTimingAnalysis : public VASTModulePass {
     VASTModulePass::getAnalysisUsage(AU);
     AU.addRequiredTransitiveID(ControlLogicSynthesisID);
     AU.addRequiredTransitive<TimingNetlist>();
-    AU.addRequiredTransitiveID(DatapathNamerID);
     AU.setPreservesAll();
   }
 
@@ -119,13 +113,17 @@ INITIALIZE_PASS_BEGIN(ExternalTimingAnalysis,
                       false, true)
   INITIALIZE_PASS_DEPENDENCY(ControlLogicSynthesis)
   INITIALIZE_PASS_DEPENDENCY(TimingNetlist)
-  INITIALIZE_PASS_DEPENDENCY(DatapathNamer)
 INITIALIZE_PASS_END(ExternalTimingAnalysis,
                     "vast-ext-timing-analysis",
                     "Perfrom External Timing Analysis",
                     false, true)
 
 void ExternalTimingAnalysis::writeNetlist(raw_ostream &Out) const {
+  // Name all expressions before writting the netlist.
+  typedef DatapathContainer::expr_iterator iterator;
+  for (iterator I = (*VM)->expr_begin(), E = (*VM)->expr_end(); I != E; ++I)
+    I->nameExpr();
+
   // Read the result from the scripting engine.
   const char *GlobalCodePath[] = { "FUs", "CommonTemplate" };
   std::string GlobalCode = getStrValueFromEngine(GlobalCodePath);
@@ -158,76 +156,205 @@ void ExternalTimingAnalysis::writeProjectScript(raw_ostream &O,
        "load_package report\n"
     << "project_new  -overwrite " << VM->getName()
     << " -family \"Cyclone IV E\" -part \"EP4CE75F29C6\" \n"
-    << "set_global_assignment -name FAMILY \"Cyclone IV E\"\n"
+       "set_global_assignment -name FAMILY \"Cyclone IV E\"\n"
        "set_global_assignment -name DEVICE EP4CE75F29C6\n"
        "set_global_assignment -name TOP_LEVEL_ENTITY " << VM->getName() << "\n"
        "set_global_assignment -name SOURCE_FILE \""<< NetlistPath.str() <<"\"\n"
        //"set_global_assignment -name SDC_FILE @SDC_FILE@\n"
        "set_global_assignment -name HDL_MESSAGE_LEVEL LEVEL1\n"
        "set_global_assignment -name SYNTH_MESSAGE_LEVEL LOW\n"
+       "set_global_assignment -name TIMEQUEST_REPORT_SCRIPT_INCLUDE_DEFAULT_ANALYSIS OFF\n"
        "export_assignments\n"
        // Start the processes.
        "execute_module -tool map\n"
-       "execute_module -tool fit -arg --early_timing_estimate\n";
-       // "execute_module -tool fit\n"
-       // "execute_module -tool sta -args {--report_script \""
-       // << ExtractScript.str() << "\"}\n";
-       // "project_close\n";
+       "execute_module -tool fit -arg --early_timing_estimate\n"
+       "execute_module -tool sta -args {--report_script \""
+        << ExtractScript.str() << "\"}\n";
+       "project_close\n";
 }
-static std::string getName(const VASTValue *V) {
-  if (const VASTNamedValue *NV = dyn_cast<VASTNamedValue>(V))
-    return NV->getName();
 
-  return cast<VASTExpr>(V)->getTempName();
+// The pair to represent the source register and the root of the combinational
+// cone.
+typedef std::pair<VASTValue*, VASTSeqValue*> SrcInfo;
+typedef std::set<SrcInfo> PathInfo;
+// The path information for each node in the combinational cone.
+typedef std::map<VASTValue*, PathInfo> ConeInfo;
+
+static void propagateSrcInfo(ConeInfo &CI, VASTValue *V) {
+  VASTOperandList *L = VASTOperandList::GetDatapathOperandList(V);
+  assert(L && "Bad Value!");
+
+  PathInfo &PI = CI[V];
+
+  typedef VASTOperandList::op_iterator iterator;
+  for (iterator I = L->op_begin(), E = L->op_end(); I != E; ++I) {
+    VASTValPtr Op = *I;
+    ConeInfo::iterator at = CI.find(Op.get());
+    // TODO: Assert Op is the leaf of the cone.
+    if (at == CI.end()) continue;
+
+    // Propagate the source information from the operand.
+    PI.insert(at->second.begin(), at->second.end());
+  }
+}
+
+static void buildPathInfoForCone(ConeInfo &CI, VASTValue *Root) {
+  // Do nothing if the cone is visited.
+  if (CI.count(Root)) return;
+
+  VASTOperandList *L = VASTOperandList::GetDatapathOperandList(Root);
+
+  if (L == 0) {
+    // Insert the trivial path information.
+    if (VASTSeqValue *V = dyn_cast<VASTSeqValue>(Root))
+      CI[V].insert(SrcInfo(Root, V));
+
+    return;
+  }
+
+  typedef  VASTOperandList::op_iterator ChildIt;
+
+  std::vector<std::pair<VASTValue*, ChildIt> > VisitStack;
+  VisitStack.push_back(std::make_pair(Root, L->op_begin()));
+
+  while (!VisitStack.empty()) {
+    VASTValue *Node = VisitStack.back().first;
+    ChildIt It = VisitStack.back().second;
+
+    // All sources of this node is visited, now propagete the source information.
+    if (It ==  VASTOperandList::GetDatapathOperandList(Node)->op_end()) {
+      VisitStack.pop_back();
+
+      propagateSrcInfo(CI, Node);
+      continue;
+    }
+
+    // Otherwise, remember the node and visit its children first.
+    VASTValue *ChildNode = It->unwrap().get();
+    ++VisitStack.back().second;
+
+    if (VASTSeqValue *V = dyn_cast<VASTSeqValue>(ChildNode)) {
+      CI[V].insert(SrcInfo(Root, V));
+      continue;
+    }
+
+    if (VASTOperandList *L = VASTOperandList::GetDatapathOperandList(ChildNode)){
+      // Mark the ChildNode as visited.
+      bool inserted = CI.insert(std::make_pair(ChildNode, PathInfo())).second;
+
+      // Do not visit the same node more than once.
+      if (!inserted) continue;
+
+      VisitStack.push_back(std::make_pair(ChildNode, L->op_begin()));
+    }
+  }
+}
+
+static std::string GetObjectName(const VASTSelector *Sel) {
+  std::string Name;
+  raw_string_ostream OS(Name);
+
+  if (const VASTBlockRAM *RAM = dyn_cast<VASTBlockRAM>(Sel->getParent())) {
+    OS << " *"
+      // BlockRam name with prefix
+      << getFUDesc<VFUBRAM>()->Prefix
+      << VFUBRAM::getArrayName(RAM->getBlockRAMNum()) << "* *"
+      // Or simply the name of the output register.
+      << VFUBRAM::getArrayName(RAM->getBlockRAMNum())
+      << "* ";
+  } else
+    OS << " *" << Sel->getName() << "* ";
+
+  return OS.str();
+}
+
+static std::string GetObjectName(const VASTValue *V) {
+  std::string Name;
+  raw_string_ostream OS(Name);
+  if (const VASTNamedValue *NV = dyn_cast<VASTNamedValue>(V)) {
+    if (const VASTSeqValue *SV = dyn_cast<VASTSeqValue>(NV))
+      return GetObjectName(SV->getSelector());
+
+    // The block RAM should be printed as Prefix + ArrayName in the script.
+    if (const char *N = NV->getName()) {
+      OS << " *" << N << "* ";
+      return OS.str();
+    }
+  } else if (const VASTExpr *E = dyn_cast<VASTExpr>(V)) {
+    std::string Name = E->getSubModName();
+    if (!Name.empty()) {
+      OS << " *" << Name << "* ";
+      return OS.str();
+    } else if (E->hasName()) {
+      OS << " *" << E->getTempName() << "* ";
+      return OS.str();
+    }
+  }
+
+  return "";
 }
 
 static void setTerminatorCollection(raw_ostream & O, const VASTSeqValue *V,
-                                    const char *CollectionName) {
-  O << "set " << CollectionName << " [" << "get_keepers \"*" << V->getName()
-    << "*\"]\n";
+  const char *CollectionName) {
+    O << "set " << CollectionName << " [" << "get_keepers \"*" << V->getName()
+      << "*\"]\n";
 }
 
-void ExternalTimingAnalysis::extractTimingForPair(raw_ostream &O,
-                                                  const VASTSeqValue *Dst,
-                                                  const VASTValue *Thu,
-                                                  const VASTSeqValue *Src)
-                                                  const {
+
+static void extractTimingForPath(raw_ostream &O, VASTSelector *Dst,
+                                 VASTValue *Thu, VASTSeqValue *Src) {
   // Get the source and destination nodes.
-  setTerminatorCollection(O, Dst, "dst");
-  setTerminatorCollection(O, Src, "src");
-  
-  O << "set " << "thu" << " [" << "get_nets \"*" << getName(Thu) << "*\"]\n";
-  
+  O << "set dst [ get_keepers \"" << GetObjectName(Dst)  << "\"]\n";
+  O << "set src [ get_keepers \"" << GetObjectName(Src)  << "\"]\n";
+  O << "set thu [ get_nets \"" << GetObjectName(Thu) << "\"]\n";
+
   O <<
-    "set paths [get_timing_paths -from $src -to $dst -setup -npath 1 -detail path_only]\n"
+    "set paths [get_timing_paths -from $src -to $dst -through $thu -setup -npath 1 -detail path_only]\n"
     "set delay -1\n"
     // Only extract the delay from source to destination when these node are
     // not optimized.
     "if {[get_collection_size $src] && [get_collection_size $dst] && [get_collection_size $paths]} {\n"
     "  foreach_in_collection path $paths {\n"
     "    set delay [get_path_info $path -data_delay]\n"
-    "    post_message -type info \"" << intptr_t(Src) << ' '<< Src->getName()
-    << " -> " << intptr_t(Dst) << ' '<< Dst->getName() << ' '
-    << getName(Dst) << " delay: $delay\"\n"
+    "    post_message -type info \"" << GetObjectName(Src)
+    << " -> " << GetObjectName(Dst) << " delay: $delay\"\n"
     "  }\n"
     "} else {\n"
-    "    post_message -type info \"" << intptr_t(Src)<< ' ' << Src->getName()
-    << " -> " << intptr_t(Dst) << ' ' << Dst->getName() << ' '
-    << getName(Dst) << " path not found!\"\n"
-    "}\n"
-    "puts $JSONFile \"\\{\\\"from\\\":" << intptr_t(Src) << ",\\\"to\\\":"
-    <<  intptr_t(Dst) << ",\\\"delay\\\":$delay\\},\"\n";
+    "    post_message -type info \"" << GetObjectName(Src)<<  " -> "
+    << GetObjectName(Dst) << " path not found!\"\n"
+    "}\n";
+    // "puts $JSONFile \"\\{\\\"from\\\":" << GetObjectName(Src) << ",\\\"to\\\":"
+    // <<  GetObjectName(Dst) << ",\\\"delay\\\":$delay\\},\"\n";
 }
 
-void ExternalTimingAnalysis::extractTimingToDst(raw_ostream &O,
-                                                const VASTSeqValue *Dst,
-                                                const VASTValue *Thu,
-                                                const SrcDelayInfo &Paths) const
-{
-  typedef TimingNetlist::src_iterator iterator;
-  for (iterator I = Paths.begin(), E = Paths.end(); I != E; ++I) {
-    if (VASTSeqValue *SeqVal = dyn_cast<VASTSeqValue>(I->first))
-      extractTimingForPair(O, Dst, Thu, SeqVal);
+static void extractTimingForSelector(raw_ostream &O, VASTSelector *Sel) {
+  ConeInfo CI;
+
+  typedef VASTSelector::iterator fanin_iterator;
+  for (fanin_iterator FI = Sel->begin(), FE = Sel->end(); FI != FE; ++FI) {
+    VASTLatch U = *FI;
+    // Visit the cone rooted on the fanin.
+    buildPathInfoForCone(CI, VASTValPtr(U).get());
+    // Visit the cone rooted on the guarding condition.
+    buildPathInfoForCone(CI, VASTValPtr(U.getPred()).get());
+    // Ignore the slot active because it is just the expression from the ready
+    // signal, and we cannot do anything with the ready signal.
+  }
+
+  // Extract the delay of the paths in cone.
+  typedef ConeInfo::iterator iterator;
+  for (iterator I = CI.begin(), E = CI.end(); I != E; ++I) {
+    VASTValue *V = I->first;
+    DEBUG(dbgs() << GetObjectName(V) << '\n');
+
+    PathInfo &Srcs = I->second;
+    typedef PathInfo::iterator src_iterator;
+    for (src_iterator SI = Srcs.begin(), SE = Srcs.end(); SI != SE; ++SI) {
+      DEBUG(dbgs().indent(2) << "Root: " << GetObjectName(SI->first)
+                             << " Src: " << GetObjectName(SI->second)
+                             << '\n');
+      extractTimingForPath(O, Sel, V, SI->second);
+    }
   }
 }
 
@@ -240,6 +367,10 @@ void ExternalTimingAnalysis::writeTimingExtractionScript(raw_ostream &O,
   // Open the file and start the array.
        "set JSONFile [open \"" << ResultPath.str() <<"\" w+]\n"
        "puts $JSONFile \"\\[\"\n";
+
+  typedef VASTModule::selector_iterator iterator;
+  for (iterator I = VM->selector_begin(), E = VM->selector_end(); I != E; ++I)
+    extractTimingForSelector(O, I);
 
   // Close the array and the file object.
   O << "puts $JSONFile \"\\{\\\"from\\\":0,\\\"to\\\":0,\\\"delay\\\":0\\}\"\n"
@@ -344,7 +475,7 @@ bool ExternalTimingAnalysis::readTimingAnalysisResult(const sys::Path &ResultPat
   typedef SequenceNode::iterator timing_it;
   for (timing_it I = DelayArray->begin(), E = DelayArray->end(); I != E; ++I) {
     MappingNode *N = cast<MappingNode>(I.operator Node *());
-    
+
     // Parse the input-to-output delay record.
     readPathDelay(N);
   }
@@ -366,7 +497,7 @@ bool ExternalTimingAnalysis::runOnVASTModule(VASTModule &M) {
   errs() << "Writing '" << Netlist.str() << "'... ";
 
   raw_fd_ostream NetlistO(Netlist.c_str(), ErrorInfo);
-  
+
   if (!ErrorInfo.empty())  return exitWithError(Netlist);
 
   // Write the netlist.
