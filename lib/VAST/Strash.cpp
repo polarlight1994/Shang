c1288dc884e4821f6cdc2f1dcbad3a337b23415f
ether <etherzhhb@gmail.com>
2013-06-05 23:09:15 +0800

    Refactor: Allow customize the leaf profiling function in Strash.

--- lib/VAST/Strash.cpp
+++ lib/VAST/Strash.cpp
@@ -26,143 +26,145 @@
 using namespace llvm;
 
 namespace llvm {
+struct StrashNode : public FoldingSetNode {
+  /// FastID - A reference to an Interned FoldingSetNodeID for this node.
+  /// The StrashTable's BumpPtrAllocator holds the data.
+  FoldingSetNodeIDRef FastID;
+
+  unsigned ID;
+
+  StrashNode(const FoldingSetNodeIDRef IDRef, unsigned ID)
+    : FastID(IDRef), ID(ID) {}
+
+  operator unsigned() const { return ID; }
+};
+
+template<typename SubClass>
 class StrashTable {
   typedef CachedStrashTable::CacheTy CacheTy;
 
-  struct Node : public FoldingSetNode {
-    /// FastID - A reference to an Interned FoldingSetNodeID for this node.
-    /// The StrashTable's BumpPtrAllocator holds the data.
-    FoldingSetNodeIDRef FastID;
-
-    unsigned ID;
-
-    Node(const FoldingSetNodeIDRef IDRef, unsigned ID)
-      : FastID(IDRef), ID(ID) {}
-
-    operator unsigned() const { return ID; }
-  };
-  friend struct FoldingSetTrait<StrashTable::Node>;
+  friend struct FoldingSetTrait<StrashNode>;
 
   unsigned LastID;
-  FoldingSet<Node> Set;
+  FoldingSet<StrashNode> Set;
   BumpPtrAllocator Allocator;
 
-  void calculateLeafID(VASTValue *Ptr, FoldingSetNodeID &ID);
-  void calculateExprID(VASTExpr *Expr, FoldingSetNodeID &ID, CacheTy &Cache);
-  void calculateID(VASTValue *Ptr, FoldingSetNodeID &ID, CacheTy &Cache);
-  void calculateID(VASTValPtr Ptr, FoldingSetNodeID &ID, CacheTy &Cache);
+  void profileLeaf(VASTValue *Ptr, FoldingSetNodeID &ID) {
+    if (VASTImmediate *Imm = dyn_cast<VASTImmediate>(Ptr)) {
+      ID.Add(Imm->getAPInt());
+      return;
+    }
+
+    static_cast<SubClass*>(this)->profileNonTrivialLeaf(Ptr, ID);
+  }
+
+  void profileExpr(VASTExpr *Expr, FoldingSetNodeID &ID, CacheTy &Cache) {
+    VASTExpr::Opcode Opcode = Expr->getOpcode();
+    ID.AddInteger(Opcode);
+    ID.AddInteger(Expr->UB);
+    ID.AddInteger(Expr->LB);
+    //ID.AddInteger(Expr->size());
+
+    SmallVector<unsigned, 8> Operands;
+
+    for (unsigned i = 0; i < Expr->size(); ++i) {
+      VASTValPtr Operand = Expr->getOperand(i);
+      unsigned NodeID = getOrCreateStrashID(Operand, Cache);
+      //unsigned Data = (NodeID << 8) | (Operand->getBitWidth() & 0xff);
+      //assert((Data >> 8) == NodeID && "NodeID overflow!");
+      Operands.push_back(NodeID);
+    }
+
+    // Sort the operands of commutative expressions
+    if (Opcode == VASTExpr::dpAnd || Opcode == VASTExpr::dpAdd
+        || Opcode == VASTExpr::dpMul)
+      array_pod_sort(Operands.begin(), Operands.end());  
+
+    for (unsigned i = 0, e = Operands.size(); i < e; ++i)
+      ID.AddInteger(Operands[i]);
+  }
+
+  void profile(VASTValue *Ptr, FoldingSetNodeID &ID, CacheTy &Cache) {
+    if (VASTExpr *E = dyn_cast<VASTExpr>(Ptr)) {
+      profileExpr(E, ID, Cache);
+      return;
+    }
+
+    profileLeaf(Ptr, ID);
+  }
+
+  void profile(VASTValPtr Ptr, FoldingSetNodeID &ID, CacheTy &Cache) {
+    unsigned NodeType = Ptr->getASTType();
+    unsigned Data = (NodeType & 0x1f) | ((Ptr.isInverted() ? 0x1 : 0x0) << 5);
+    assert(NodeType == (Data & 0x1f) && "NodeType overflow!");
+    ID.AddInteger(Data);
+    profile(Ptr.get(), ID, Cache);
+  }
 
 public:
   StrashTable() : LastID(0) {}
 
-  unsigned getOrCreateStrashID(VASTValPtr Ptr, CacheTy &Cache);
-};
+  unsigned getOrCreateStrashID(VASTValPtr Ptr, CacheTy &Cache) {
+    if (unsigned NodeId = Cache.lookup(Ptr))
+      return NodeId;
 
-// Specialize FoldingSetTrait for StrashNode to avoid needing to compute
-// temporary FoldingSetNodeID values.
-template<> struct FoldingSetTrait<StrashTable::Node> {
-  static void Profile(const StrashTable::Node &X, FoldingSetNodeID& ID) {
-    ID = X.FastID;
-  }
-  static bool Equals(const StrashTable::Node &X, const FoldingSetNodeID &ID,
-                     unsigned IDHash, FoldingSetNodeID &TempID) {
-    return ID == X.FastID;
-  }
+    // Now look it up in the Hash Table. 
+    FoldingSetNodeID ID;
+    profile(Ptr, ID, Cache);
 
-  static
-  unsigned ComputeHash(const StrashTable::Node &X, FoldingSetNodeID &TempID) {
-    return X.FastID.ComputeHash();
-  }
-};
+    void *IP = 0;
+    if (StrashNode *N = Set.FindNodeOrInsertPos(ID, IP)) {
+      unsigned NodeId = unsigned(*N);
+      assert(NodeId && "Bad ID!");
+      Cache.insert(std::make_pair(Ptr, NodeId));
+      return NodeId;
+    }
 
-struct Strash : public ImmutablePass {
-  static char ID;
-  StrashTable Table;
-  Strash();
-};
+    StrashNode *N = new (Allocator) StrashNode(ID.Intern(Allocator), ++LastID);
+    Set.InsertNode(N, IP);
 
-void initializeStrashPass(PassRegistry &Registry);
-}
-
-void StrashTable::calculateLeafID(VASTValue *Ptr, FoldingSetNodeID &ID) {
-  if (VASTNamedValue *NV = dyn_cast<VASTNamedValue>(Ptr)) {
-    ID.AddString(NV->getName());
-    return;
-  }
-
-  VASTImmediate *Imm = cast<VASTImmediate>(Ptr);
-  ID.Add(Imm->getAPInt());
-}
-
-void StrashTable::calculateExprID(VASTExpr *Expr, FoldingSetNodeID &ID,
-                                  DenseMap<VASTValPtr, unsigned> &Cache) {
-  VASTExpr::Opcode Opcode = Expr->getOpcode();
-  ID.AddInteger(Opcode);
-  ID.AddInteger(Expr->UB);
-  ID.AddInteger(Expr->LB);
-  //ID.AddInteger(Expr->size());
-
-  SmallVector<unsigned, 8> Operands;
-
-  for (unsigned i = 0; i < Expr->size(); ++i) {
-    VASTValPtr Operand = Expr->getOperand(i);
-    unsigned NodeID = getOrCreateStrashID(Operand, Cache);
-    //unsigned Data = (NodeID << 8) | (Operand->getBitWidth() & 0xff);
-    //assert((Data >> 8) == NodeID && "NodeID overflow!");
-    Operands.push_back(NodeID);
-  }
-
-  // Sort the operands of commutative expressions
-  if (Opcode == VASTExpr::dpAnd || Opcode == VASTExpr::dpAdd
-      || Opcode == VASTExpr::dpMul)
-    array_pod_sort(Operands.begin(), Operands.end());  
-
-  for (unsigned i = 0, e = Operands.size(); i < e; ++i)
-    ID.AddInteger(Operands[i]);
-}
-
-void
-StrashTable::calculateID(VASTValue *Ptr, FoldingSetNodeID &ID, CacheTy &Cache) {
-  if (VASTExpr *E = dyn_cast<VASTExpr>(Ptr)) {
-    calculateExprID(E, ID, Cache);
-    return;
-  }
-
-  calculateLeafID(Ptr, ID);
-}
-
-void
-StrashTable::calculateID(VASTValPtr Ptr, FoldingSetNodeID &ID, CacheTy &Cache) {
-  unsigned NodeType = Ptr->getASTType();
-  unsigned Data = (NodeType & 0x1f) | ((Ptr.isInverted() ? 0x1 : 0x0) << 5);
-  assert(NodeType == (Data & 0x1f) && "NodeType overflow!");
-  ID.AddInteger(Data);
-  calculateID(Ptr.get(), ID, Cache);
-}
-
-unsigned StrashTable::getOrCreateStrashID(VASTValPtr Ptr, CacheTy &Cache) {
-  if (unsigned NodeId = Cache.lookup(Ptr))
-    return NodeId;
-  
-  // Now look it up in the Hash Table. 
-  FoldingSetNodeID ID;
-  calculateID(Ptr, ID, Cache);
-
-  void *IP = 0;
-  if (Node *N = Set.FindNodeOrInsertPos(ID, IP)) {
     unsigned NodeId = unsigned(*N);
     assert(NodeId && "Bad ID!");
     Cache.insert(std::make_pair(Ptr, NodeId));
     return NodeId;
   }
 
-  Node *N = new (Allocator) Node(ID.Intern(Allocator), ++LastID);
-  Set.InsertNode(N, IP);
+  void reset() {
+    Set.clear();
+    Allocator.Reset();
+    LastID = 0;
+  }
+};
 
-  unsigned NodeId = unsigned(*N);
-  assert(NodeId && "Bad ID!");
-  Cache.insert(std::make_pair(Ptr, NodeId));
-  return NodeId;
+// Specialize FoldingSetTrait for StrashNode to avoid needing to compute
+// temporary FoldingSetNodeID values.
+template<> struct FoldingSetTrait<StrashNode> {
+  static void Profile(const StrashNode &X, FoldingSetNodeID& ID) {
+    ID = X.FastID;
+  }
+  static bool Equals(const StrashNode &X, const FoldingSetNodeID &ID,
+                     unsigned IDHash, FoldingSetNodeID &TempID) {
+    return ID == X.FastID;
+  }
+
+  static
+  unsigned ComputeHash(const StrashNode &X, FoldingSetNodeID &TempID) {
+    return X.FastID.ComputeHash();
+  }
+};
+
+struct Strash : public ImmutablePass, public StrashTable<Strash> {
+  static char ID;
+
+  Strash();
+
+  void profileNonTrivialLeaf(VASTValue *Ptr, FoldingSetNodeID &ID) {
+    VASTNamedValue *NV = dyn_cast<VASTNamedValue>(Ptr);
+    ID.AddString(NV->getName());
+  }
+};
+
+void initializeStrashPass(PassRegistry &Registry);
 }
 
 Strash::Strash() : ImmutablePass(ID) {
@@ -206,5 +208,5 @@ void CachedStrashTable::releaseMemory() {
 }
 
 unsigned CachedStrashTable::getOrCreateStrashID(VASTValPtr Ptr) {
-  return Table->Table.getOrCreateStrashID(Ptr, Cache);
+  return Table->getOrCreateStrashID(Ptr, Cache);
 }
