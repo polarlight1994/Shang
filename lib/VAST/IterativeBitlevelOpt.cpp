8d715d15ab4748a1bd2306e72cc4f0a7c49aa09c
ether <etherzhhb@gmail.com>
2013-06-28 15:46:21 +0800

    Add the source file of iterative bit-level optimizations.

---
+++ lib/VAST/IterativeBitlevelOpt.cpp
@@ -0,0 +1,14 @@
+//==- IterativeBitlevelOpt.cpp - Iterative Bit-level Optimization -*- C++ -*-=//
+//
+//                      The Shang HLS frameowrk                               //
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implement the IterativeBitLevelOpt pass.
+// The IterativeBitLevelOpt pass perform the bit-level optimizations iteratively
+// until the bit-level optimization do not optimize the Module any further.
+//
+//===----------------------------------------------------------------------===//
