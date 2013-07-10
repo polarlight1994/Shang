7c40b9581db6f8c0617ebfe35a9af061d96ad39f
ether <etherzhhb@gmail.com>
2013-07-10 23:18:24 +0800

    Test divide by constant.

---
+++ testsuite/SimpleTest/op_sdiv_const.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int op_sdiv_const(int a) __attribute__ ((noinline));
+int op_sdiv_const(int a) { return a / 411774; }
+#ifdef __cplusplus
+}
+#endif
+
+int main(int argc, char **argv) {
+  srand (16);
+
+  int i;
+  for(i = 0; i < 16; ++i) {
+    int a = rand();
+    printf("result:%d\n", op_sdiv_const(a));
+  }
+
+  return 0;
+}
