71ecdbc8199e41a22eed5d680c8414e6eeb03698
ether <etherzhhb@gmail.com>
2013-12-03 17:04:23 +0800

    Temprorary disable the scheduling pass, it will not work unless we have the  hierarchy CFG.

--- shang.cpp
+++ shang.cpp
@@ -292,7 +292,7 @@ int main(int argc, char **argv) {
     // Perform the scheduling.
     HLSPasses.add(createDataflowAnnotationPass());
     HLSPasses.add(createScalarEvolutionAliasAnalysisPass());
-    HLSPasses.add(createIterativeSchedulingPass());
+    DEBUG(HLSPasses.add(createIterativeSchedulingPass()));
     // Scheduling will restruct the datapath. Optimize the datapath again
     // after scheduling.
     HLSPasses.add(createLUTMappingPass());
