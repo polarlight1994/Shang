polarlight1994/Shang

commit fd2ba774433a74b1729bc6666ec013ee7c4afae5

Refactor: Implement the self-contain erase from parent for VASTSeqOp.

lib/VAST/VASTCtrlRgn.cpp
@@ -27,19 +27,6 @@
 
 using namespace llvm;
 
-void VASTCtrlRgn::eraseSeqOp(VASTSeqOp *SeqOp) {
-  if (SeqOp->getSlot())
-    SeqOp->removeFromParent();
-
-  for (unsigned i = 0, e = SeqOp->num_srcs(); i != e; ++i) {
-    VASTLatch U = SeqOp->getSrc(i);
-    U.removeFromParent();
-  }
-
-  SeqOp->dropUses();
-  Ops.erase(SeqOp);
-}
-
 bool VASTCtrlRgn::gc() {
   bool Changed = false;
 
@@ -49,7 +36,7 @@ bool VASTCtrlRgn::gc() {
       DEBUG(dbgs() << "Removing SeqOp whose predicate is always false:\n";
             Op->dump(););
 
-      eraseSeqOp(Op);
+      Ops.erase(Op);
 
       Changed |= true;
     }
@@ -97,7 +84,8 @@ VASTCtrlRgn::createSlot(unsigned SlotNum, BasicBlock *ParentBB,
   assert(!std::count_if(Slots.begin(), Slots.end(), SlotNumEq(SlotNum))
          && "The same slot had already been created!");
 
-  VASTSlot *Slot = new VASTSlot(SlotNum, ParentBB, Pred, IsVirtual, Schedule);
+  VASTSlot *Slot = new VASTSlot(SlotNum, *this, ParentBB,
+                                Pred, IsVirtual, Schedule);
   // Insert the newly created slot before the finish slot.
   Slots.insert(Slots.back(), Slot);
 
@@ -106,10 +94,11 @@ VASTCtrlRgn::createSlot(unsigned SlotNum, BasicBlock *ParentBB,
 
 VASTSlot *VASTCtrlRgn::createLandingSlot() {
   BasicBlock *Entry = NULL/*getEntryBlock()*/;
-  VASTSlot *Landing = new VASTSlot(0, Entry, VASTImmediate::True, false, 0);
+  VASTSlot *Landing = new VASTSlot(0, *this, Entry,
+                                   VASTImmediate::True, false, 0);
   Slots.push_back(Landing);
   // Also create the finish slot.
-  Slots.push_back(new VASTSlot(-1));
+  Slots.push_back(new VASTSlot(-1, *this));
   return Landing;
 }
 
@@ -208,8 +197,9 @@ void VASTCtrlRgn::finalize() {
   for (seqop_iterator I = seqop_begin(), E = seqop_end(); I != E; ++I)
     I->dropOperands();
 
-  Ops.clear();
+  // Delete all slot, this also implicitly delete all operation.
   Slots.clear();
+  assert(Ops.empty() && "Operations are not deleted with its parent slots?");
 }
 
 VASTCtrlRgn::~VASTCtrlRgn() {

include/vast/VASTCtrlRgn.h
@@ -78,7 +78,7 @@ public:
   const_slot_iterator slot_begin() const { return Slots.begin(); }
   const_slot_iterator slot_end() const { return Slots.end(); }
 
-  ilist<VASTSlot> &getSLotList() { return Slots; }
+  SlotListTy &getSLotList() { return Slots; }
 
   VASTSlot *createSlot(unsigned SlotNum, BasicBlock *ParentBB, unsigned Schedule,
                        VASTValPtr Pred = VASTImmediate::True,
@@ -111,15 +111,12 @@ public:
   VASTSlotCtrl *createStateTransition(VASTNode *N, VASTSlot *Slot,
                                       VASTValPtr GuardCnd);
 
-  /// Remove the VASTSeqOp from the module and delete it. Please note that
-  /// the SeqOp should be remove from its parent slot before we erase it.
-  void eraseSeqOp(VASTSeqOp *SeqOp);
-
   // Iterate over all operations in current control region.
   typedef SeqOpListTy::iterator seqop_iterator;
   typedef SeqOpListTy::const_iterator const_seqop_iterator;
   seqop_iterator seqop_begin() { return Ops.begin(); }
   seqop_iterator seqop_end() { return Ops.end(); }
+  SeqOpListTy &getOpList() { return Ops; }
 
   void print(raw_ostream &OS) const;
 

include/vast/VASTSeqOp.h
@@ -80,8 +80,8 @@ class VASTSeqOp : public VASTOperandList, public VASTNode,
 
   void eraseOperand(unsigned Idx);
 
-  void operator=(const VASTSeqOp &RHS); // DO NOT IMPLEMENT
-  VASTSeqOp(const VASTSeqOp &RHS); // DO NOT IMPLEMENT
+  void operator=(const VASTSeqOp &RHS) LLVM_DELETED_FUNCTION;
+  VASTSeqOp(const VASTSeqOp &RHS) LLVM_DELETED_FUNCTION;
 
 protected:
   VASTSeqOp(VASTTypes T, VASTSlot *S, bool UseSlotActive, unsigned Size);
@@ -94,7 +94,6 @@ public:
 
   // Set the parent slot to 0.
   void clearParent() { S.setPointer(0); }
-  void removeFromParent();
 
   unsigned getSlotNum() const;
 
@@ -136,6 +135,9 @@ public:
 
   // Provide the < operator to support set of VASTSeqDef.
   bool operator<(const VASTSeqOp &RHS) const;
+
+  void eraseFromParentList(ilist<VASTSeqOp> &List);
+  void eraseFromParent();
 
   virtual void dropUses();
 

include/vast/VASTSlot.h
@@ -29,6 +29,7 @@ class vlang_raw_ostream;
 class VASTSlot;
 class VASTSeqOp;
 class VASTSlotCtrl;
+class VASTCtrlRgn;
 
 class VASTSlot : public VASTNode, public ilist_node<VASTSlot> {
 public:
@@ -68,6 +69,9 @@ public:
   typedef PredVecTy::const_iterator const_pred_iterator;
 
 private:
+  // The parent control region of the current slot.
+  VASTCtrlRgn &R;
+
   // The relative signal of the slot: Slot register, Slot active and Slot ready.
   VASTUse SlotReg;
   VASTUse SlotActive;
@@ -86,11 +90,11 @@ private:
   }
 
   friend class VASTCtrlRgn;
-  VASTSlot(unsigned slotNum, BasicBlock *ParentBB, VASTValPtr Pred,
-           bool IsSubGrp, unsigned Schedule);
-
-  VASTSlot() : VASTNode(vastSlot), SlotReg(this), SlotActive(this),
-    SlotGuard(this), SlotNum(0), IsSubGrp(true), Schedule(0) {}
+  VASTSlot(unsigned slotNum, VASTCtrlRgn &R, BasicBlock *ParentBB,
+           VASTValPtr Pred, bool IsSubGrp, unsigned Schedule);
+  // Create the finish slot.
+  VASTSlot(unsigned SlotNum, VASTCtrlRgn &R);
+  VASTSlot();
 
   friend struct ilist_sentinel_traits<VASTSlot>;
 
@@ -144,8 +148,6 @@ public:
     }
   };
 
-  // Create the finish slot.
-  explicit VASTSlot(unsigned SlotNum);
   ~VASTSlot();
 
   typedef uint16_t SlotNumTy;
@@ -157,6 +159,7 @@ public:
   void copySignals(VASTSlot *S);
 
   BasicBlock *getParent() const;
+  VASTCtrlRgn &getParentRgn() const { return R; }
 
   void print(raw_ostream &OS) const;
 

lib/HighlevelSynthesis/Binding/RegisterSharing.cpp
@@ -336,7 +336,7 @@ void RegisterSharing::mergeSelector(VASTSelector *To, VASTSelector *From,
   // Erase the dead ops.
   while (!DeadOps.empty()) {
     VASTSeqOp *Op = DeadOps.pop_back_val();
-    VM.eraseSeqOp(Op);
+    Op->eraseFromParent();
   }
 
   VM.eraseSelector(From);

lib/HighlevelSynthesis/Scheduling/ScheduleEmitter.cpp
@@ -90,7 +90,6 @@ class ScheduleEmitter : public MinimalExprBuilderContext {
   unsigned CurrentSlotNum;
 
   void clearUp();
-  void clearUp(VASTSlot *S);
 
   VASTSlot *createSlot(BasicBlock *BB, unsigned Schedule) {
     ++NumSlots;
@@ -152,27 +151,6 @@ ScheduleEmitter::ScheduleEmitter(VASTModule &VM, VASTSchedGraph &G)
   : MinimalExprBuilderContext(VM), Builder(*this), VM(VM), G(G),
     CurrentSlotNum(0) {}
 
-void ScheduleEmitter::clearUp(VASTSlot *S) {
-  typedef VASTSlot::op_iterator op_iterator;
-  for (op_iterator I = S->op_begin(); I != S->op_end(); /*++I*/) {
-    VASTSeqOp *SeqOp = *I;
-
-    // Delete the dead Exprs used by this SeqOp.
-    for (unsigned i = 0, e = SeqOp->size(); i != e; ++i) {
-      VASTValue *V = SeqOp->getOperand(i).unwrap().get();
-
-      SeqOp->getOperand(i).unlinkUseFromUser();
-
-      if (!V->use_empty()) continue;
-
-      if (VASTExpr *Child = dyn_cast<VASTExpr>(V))
-        VM.recursivelyDeleteTriviallyDeadExprs(Child);
-    }
-
-    I = S->removeOp(I);
-    VM.eraseSeqOp(SeqOp);
-  }
-}
 
 void ScheduleEmitter::clearUp() {
   // The selectors will become invalid after we regenerate the STG, drop them
@@ -182,13 +160,7 @@ void ScheduleEmitter::clearUp() {
     I->dropMux();
 
   // Clear up the VASTSeqOp in the old list.
-  while (!OldSlots.empty()) {
-    VASTSlot *CurSlot = &OldSlots.back();
-
-    clearUp(CurSlot);
-
-    OldSlots.pop_back();
-  }
+  OldSlots.clear();
 
   // Release the dead objects now.
   VM.gc();

lib/HighlevelSynthesis/SelectorPipelining.cpp
@@ -226,7 +226,7 @@ void SelectorPipelining::descomposeSeqInst(VASTSeqInst *SeqInst) {
       ->addSrc(VASTValPtr(L), 0, L.getSelector(), L.getDst());
   }
 
-  VM->eraseSeqOp(SeqInst);
+  SeqInst->eraseFromParent();
 }
 
 //static bool isLatchIdentical(const VASTLatch &LHS, const VASTLatch &RHS) {

lib/VAST/VASTModule.cpp
@@ -577,7 +577,7 @@ bool VASTModule::gc() {
     while (!DeadOps.empty()) {
       VASTLatch L = DeadOps.pop_back_val();
       if (L.Op->getNumDefs() == 1)
-        eraseSeqOp(L.Op);
+        L.Op->eraseFromParent();
       else
         L.eraseOperand();
     }
@@ -587,20 +587,17 @@ bool VASTModule::gc() {
     Changed |= true;
   }
 
-  // Release the dead seqops, this happen when we fold the SeqOp through the
-  // false paths.
-  for (seqop_iterator I = seqop_begin(); I != seqop_end(); /*++I*/) {
-    VASTSeqOp *Op = I++;
-    if (Op->getGuard() == VASTImmediate::False) {
-      DEBUG(dbgs() << "Removing SeqOp whose predicate is always false:\n";
-      Op->dump(););
-
-      eraseSeqOp(Op);
-
-      Changed |= true;
-    }
+  typedef SubmoduleList::iterator submodule_iterator;
+  for (submodule_iterator I = Submodules.begin(), E = Submodules.end();
+       I != E; ++I) {
+    if (VASTCtrlRgn *R = dyn_cast<VASTCtrlRgn>(I))
+      Changed |= R->gc();
   }
 
+  Changed |= VASTCtrlRgn::gc();
+
+  Changed |= DatapathContainer::gc();
+
   // At last clear up the dead VASTExprs.
-  return DatapathContainer::gc() || Changed;
+  return Changed;
 }

commit 350f40b493b1eb41427708545feae0ff48dc97e8

Fix a bug introduced by commit fd2ba774433a74b1729bc6666ec013ee7c4afae5.
Call eraseFromParent on an operation to correctly erase the operation.

lib/VAST/VASTCtrlRgn.cpp
@@ -36,7 +36,7 @@ bool VASTCtrlRgn::gc() {
       DEBUG(dbgs() << "Removing SeqOp whose predicate is always false:\n";
             Op->dump(););
 
-      Ops.erase(Op);
+      Op->eraseFromParent();
 
       Changed |= true;
     }