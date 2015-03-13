aa31f0fc13b096b62ed13f86764b187b80cc04ec
polarlight <polarlight1994@gmail.com>
2015-03-13 10:39:36 +0800

    Bug Fixed

--- lib/SIR/SIR.cpp
+++ lib/SIR/SIR.cpp
@@ -12,6 +12,8 @@
 //===----------------------------------------------------------------------===//
 #include "sir/SIR.h"
 #include "llvm/IR/Value.h"
+#include "llvm/ADT/PostOrderIterator.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 
@@ -25,8 +27,8 @@ void SIRRegister::printDecl(raw_ostream &OS) const {
   // Need to Implement these functions.
   OS << "reg" << BitRange(getBitWidth(), 0, false);
   OS << " " << Mangle(getName());
-  // Set the IniteVal into 0;
-  OS << " = " << buildLiteral(0, getBitWidth(), false)
+  // Set the IniteVal.
+  OS << " = " << buildLiteral(InitVal, getBitWidth(), false)
      << ";\n";
 }
 
@@ -67,7 +69,7 @@ bool SIRSlot::hasNextSlot(SIRSlot *NextSlot) {
   return false;
 }
 
-void SIRSlot::addSuccSlot(SIRSlot *NextSlot, EdgeType T) {
+void SIRSlot::addSuccSlot(SIRSlot *NextSlot, EdgeType T, Value *Cnd) {
   assert(T <= 3 && "Unexpected distance!");
   // Do not add the same successor slot twice.
   if (hasNextSlot(NextSlot)) return;
@@ -77,8 +79,55 @@ void SIRSlot::addSuccSlot(SIRSlot *NextSlot, EdgeType T) {
   //assert(NextSlot != this && "Unexpected loop!");
 
   // Connect the slots.
-  NextSlot->PredSlots.push_back(EdgePtr(this, T));
-  NextSlots.push_back(EdgePtr(NextSlot, T));
+  NextSlot->PredSlots.push_back(EdgePtr(this, T, Cnd));
+  NextSlots.push_back(EdgePtr(NextSlot, T, Cnd));
+}
+
+void SIRSlot::unlinkSucc(SIRSlot *S) {
+	for (succ_iterator I = succ_begin(), E = succ_end(); I != E; ++I) {
+		if (S != *I)
+			continue;
+
+		// Remove the PredEdge in DstSlot.
+		pred_iterator at = std::find(S->PredSlots.begin(), S->PredSlots.end(), this);
+		S->PredSlots.erase(at);
+
+		// Remove the SuccEdge in SrcSlot.
+		NextSlots.erase(I);
+		return;
+	}
+
+	llvm_unreachable("Not the sucessor of this slot!");
+}
+
+void SIRSlot::unlinkSuccs() {
+	for (succ_iterator I = succ_begin(), E = succ_end(); I != E; ++I) {
+		SIRSlot *SuccSlot = *I;
+		assert(SuccSlot != this && "Unexpected loop!");
+
+		// Find the this slot in the PredSlot of the successor and erase it.
+		pred_iterator at
+			= std::find(SuccSlot->PredSlots.begin(), SuccSlot->PredSlots.end(), this);
+		SuccSlot->PredSlots.erase(at);
+	}
+
+	NextSlots.clear();
+}
+
+void SIRSlot::replaceAllUsesWith(SIRSlot *S) {
+	SmallVector<EdgePtr, 4> Succs;
+
+	// Store all the Succs of CurSlot.
+	for (succ_iterator I = this->succ_begin(), E = this->succ_end(); I != E; ++I) 
+		Succs.push_back(*I);
+	
+	// Unlink all Succs of CurSlot.
+	this->unlinkSuccs();
+
+	// Link all Succs of CurSlot to Target Slot.
+	typedef SmallVector<EdgePtr, 4>::iterator iterator;
+	for (iterator I = Succs.begin(), E = Succs.end(); I != E; ++I) 
+		S->addSuccSlot(I->getSlot(), I->getType(), I->getCnd());	
 }
 
 void SIRSlot::print(raw_ostream &OS) const {
@@ -113,9 +162,36 @@ void SIR::printModuleDecl(raw_ostream &OS) const {
   OS << ");\n";
 }
 
+void SIR::printRegDecl(raw_ostream &OS) const {
+	OS << "\n";
+
+	for (SIR::const_register_iterator I = registers_begin(), E = registers_end();
+		   I != E; ++I) {
+		(*I)->printDecl(OS.indent(2));
+	}
+}
+
 bool SIR::gcImpl() {
-	// Hack: The GC function here should be constructed
-	// according to the DeadInstElemPass in LLVM.
+	// Remove all the instructions that is not be used anymore.
+	Function *F = getFunction();
+
+	// Visit the basic block in topological order.
+	ReversePostOrderTraversal<BasicBlock *> RPO(&F->getEntryBlock());
+	typedef ReversePostOrderTraversal<BasicBlock *>::rpo_iterator bb_top_iterator;
+
+	for (bb_top_iterator BBI = RPO.begin(), BBE = RPO.end(); BBI != BBE; ++BBI) {
+		BasicBlock *BB = *BBI;
+		typedef BasicBlock::iterator iterator;
+		for (iterator InstI = BB->begin(), InstE = BB->end(); InstI != InstE; ++InstI) {
+			Instruction *Inst = InstI;
+
+			if (Inst->use_empty() && !isa<ReturnInst>(Inst)) {
+				Inst->eraseFromParent();
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 
@@ -123,8 +199,8 @@ bool SIR::gc() {
 	bool changed = false;
 
 	// Iteratively release the dead objects.
-	while (gcImpl())
-		changed = true;
+ 	while (gcImpl())
+ 		changed = true;
 
 	return changed;
 }
