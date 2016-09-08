0fa3dff678ef456439cb33f894152dc54a4fcad6
[polar] <[971471059@qq.com]>
2016-09-08 15:24:57 +0800

    Remove the benchmark Multi_Operand_Adder

--- testsuite/benchmark/Mutil_Operand_Adder/3/moa3.cpp
+++
@@ -1,4 +0,0 @@
-int moa3(int operand_0, int operand_1, int operand_2) {
-  return operand_0 + operand_1 + operand_2;
-}
-
--- testsuite/benchmark/Mutil_Operand_Adder/6/moa6.cpp
+++
@@ -1,4 +0,0 @@
-int moa6(int operand_0, int operand_1, int operand_2, int operand_3, int operand_4, int operand_5) {
-  return operand_0 + operand_1 + operand_2 + operand_3 + operand_4 + operand_5;
-}
-
