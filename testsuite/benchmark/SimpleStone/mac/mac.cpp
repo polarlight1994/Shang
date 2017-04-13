3579718fcc65b3c398732c02796007788a522add
[polar] <[971471059@qq.com]>
2017-04-13 17:05:24 +0800

    Reset the benchmark

--- testsuite/benchmark/SimpleStone/mac/mac.cpp
+++
@@ -1,4 +0,0 @@
-int mac(int operand_0, int operand_1, int operand_2, int operand_3, int operand_4) {
-  int mul_result = operand_0 * operand_1;
-  return mul_result + operand_2 + operand_3 + operand_4;
-}
